3d168c6975890da48cc5e7abc05f9af70e0063ec
Spencer Perry <spencerfarrallperry@gmail.com>
2019-02-02 19:59:14 -0500

    added initial postgres connection

--- app.js
+++ app.js
@@ -3,6 +3,30 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+
+const pgp = require('pg-promise')(/*options*/);
+
+var cn = {
+    host: 'localhost', // server name or IP address;
+    port: 5432,
+    database: 'database_tutorial',
+    user: 'database_tutorial',
+    password: 'supersecret'
+};
+
+var db = pgp(cn); // database instance;
+
+// select and return user name from id:
+//db.one("CREATE TABLE thought( FROM users WHERE id = $1", 123)
+//    .then(user => {
+//        console.log(user.name); // print user name;
+//    })
+//    .catch(error => {
+//        console.log(error); // print the error;
+//    });
+
+
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
@@ -12,4 +36,3 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
